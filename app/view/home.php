d62e1bda009c3c0203c4b37aad0121545c75e955
vencerConsul <a.adriansalguet3.code@gmail.com>
2023-04-04 22:34:32 +0800

    new etits

---
+++ app/view/home.php
@@ -0,0 +1,10 @@
+<section id="section_2">
+    <div class="section_2_container">
+        <div class="cover_background_2">
+        <div class="section_2_content">
+            
+        </div>
+        </div>
+        
+    </div>
+</section>
--- app/view/template/header.php
+++ app/view/template/header.php
@@ -1,64 +1,79 @@
 <?php $this->suspensionRedirect($view); ?>
 <!DOCTYPE html>
 <html lang="en" <?php $this->helpers->htmlClasses(); ?>>
+
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/> 
-	<meta name="apple-mobile-web-app-capable" content="yes" /> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
-	<?php $this->helpers->seo($view); ?> 
-	<link rel="icon" href="public/images/favicon.png" type="image/x-icon"> 
-	<link href="<?php echo URL; ?>public/styles/style.css" rel="stylesheet"> 
+	<?php $this->helpers->seo($view); ?>
+	<link rel="icon" href="public/images/favicon.png" type="image/x-icon">
+	<link href="<?php echo URL; ?>public/styles/style.css" rel="stylesheet">
 	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
 	<link rel="stylesheet" href="<?php echo URL; ?>public/fancybox/source/jquery.fancybox.css" media="screen" />
 	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 	<?php $this->helpers->analytics(); ?>
 </head>
 
-<body <?php $this->helpers->bodyClasses($view); ?>> 
-<?php $this->checkSuspensionHeader(); ?> 
+<body <?php $this->helpers->bodyClasses($view); ?>>
+	<?php $this->checkSuspensionHeader(); ?>
 
-	<header>
-		<div id="header">
-			<div class="header-main">
-				<div class="header-content">
-					<a href="#">FOLLOW US</a>
-					<img src="./public/images/icons/socmed.fw.png" alt="img">
-					<a href="#">CALL US TODAY</a>
-					
-					<a href="tel:(408) 413-9093" class="tel">
-						<img src="./public/images/icons/phone.fw.png" alt="img">
-						(408) 413-9093
-					</a>
-				</div>
-			</div>
-		</div>
 
-		<div id="menu">
-			<div class="menu-main">
-				<div class="menu-content">
-					<img src="./public/images/icons/logo.fw.png" alt="img" onclick="window.location.href='<?php echo URL; ?>'">
-					<nav>
+	<!-- <li <?php $this->helpers->isActiveMenu("home"); ?> class="btn"><a  href="home#content">HOME</a></li>
+			                <!-- <li <?php $this->helpers->isActiveMenu("about"); ?>><a href="about#content">ABOUT US</a></li> -->
+	<!-- <div class="menu_flex">
 						<ul>
-			                <li <?php $this->helpers->isActiveMenu("home"); ?> class="btn"><a  href="home#content">HOME</a></li>
-			                <li <?php $this->helpers->isActiveMenu("about"); ?>><a href="about#content">ABOUT US</a></li>
-			                <li <?php $this->helpers->isActiveMenu("services"); ?>><a href="services#content">SERVICES</a></li>
-			                <li <?php $this->helpers->isActiveMenu("gallery"); ?>><a href="gallery#content">GALLERY</a></li>
-			                <li <?php $this->helpers->isActiveMenu("reviews"); ?>><a href="reviews#content">REVIEWS</a></li>
-			                <li <?php $this->helpers->isActiveMenu("contact"); ?>><a href="contact#content">CONTACT US</a></li>
+							
 						</ul>
-					</nav>
-					<button class="hamburger">
-						<div class="bar"></div>
-					</button>
+					</div> -->
+
+	<section id="section_1">
+		<header>
+			<div id="header_container">
+				<div class="header_content">
+					<h2>call us at</h2>
+					<img src="" alt="">
+					<h3>856 - 479 - 8432</h3>
+					<span class="_header_gap"></span>
+					<img src="./public/images/common/soc_med_header.jpg" alt="soc_med">
 				</div>
 			</div>
+
+			<div id="menu_container">
+				<div class="menu_main">
+					<div class="menu_content">
+						<img src="./public/images/common/main_logo.jpg" alt="logo">
+						<nav>
+							<ul>
+								<li <?php $this->helpers->isActiveMenu("home"); ?> class="btn"><a href="home#content">HOME</a></li>
+								<li <?php $this->helpers->isActiveMenu("commercial"); ?>><a href="commercials#content">COMMERCIAL RENDERINGS</a></li>
+								<li <?php $this->helpers->isActiveMenu("residentials"); ?>><a href="residentials#content">RESIDENTIAL RENDERINGS</a></li>
+								<li <?php $this->helpers->isActiveMenu("interior"); ?>><a href="interior#content">INTERIOR RENDERINGS</a></li>
+								<li <?php $this->helpers->isActiveMenu("floorplan"); ?>><a href="floorplan#content">FLOORPLAN RENDERINGS</a></li>
+								<li <?php $this->helpers->isActiveMenu("permit"); ?>><a href="permit#content">PERMIT TO BUILD RENDERINGS</a></li>
+								<li <?php $this->helpers->isActiveMenu("contractor"); ?>><a href="contractor#content">CONTRACTOR RENDERINGS</a></li>
+								<li <?php $this->helpers->isActiveMenu("virtual"); ?>><a href="virtual#content">VIRTUAL STAGING</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</header>
+
+		<div id="home_content">
+			<div class="title">
+				<h4>A CREATIVE TEAM OF</h4>
+				<h5>Real Estate Renderings Specialist</h5>
+				<a href="#">LEARN MORE</a>
+			</div>
 		</div>
-	</header>
+	</section>
+
 	<!-- Hero -->
-	<?php if($view == "home"):?>
-		
+	<?php if ($view == "home") : ?>
+
 	<?php endif; ?>
