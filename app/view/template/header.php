21cc830964e55046c83dbb24d11c4155de13fad9
vencerConsul <a.adriansalguet3.code@gmail.com>
2023-04-06 00:04:19 +0800

    added and updated styles

--- app/view/template/header.php
+++ app/view/template/header.php
@@ -17,6 +17,10 @@
 	<link rel="stylesheet" href="<?php echo URL; ?>public/fancybox/source/jquery.fancybox.css" media="screen" />
 	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 	<?php $this->helpers->analytics(); ?>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap" rel="stylesheet">
 </head>
 
 <body <?php $this->helpers->bodyClasses($view); ?>>
@@ -31,57 +35,48 @@
 						</ul>
 					</div> -->
 
-	<section id="section_1">
-		<header>
-			<div id="header_container">
-				<div class="header_content">
-					<h2>call us at</h2>
-					<img src="" alt="">
-					<h3>856 - 479 - 8432</h3>
-					<span class="header_gap"></span>
-					<img src="./public/images/common/soc_med_header.jpg" alt="soc_med">
-				</div>
+
+	<header>
+		<div id="header_container">
+			<div class="logo">
+				<img src="./public/images/common/main_logo.png" alt="">
 			</div>
 
-			<div id="menu_container">
-				<div class="menu_main">
-					<div class="menu_content">
-						<img src="./public/images/common/main_logo.jpg" alt="logo">
-						<nav>
-							<ul>
-								<li <?php $this->helpers->isActiveMenu("home"); ?> class="btn"><a
-										href="home#content">HOME</a></li>
-								<li <?php $this->helpers->isActiveMenu("commercial"); ?>><a
-										href="commercials#content">COMMERCIAL RENDERINGS</a></li>
-								<li <?php $this->helpers->isActiveMenu("residentials"); ?>><a
-										href="residentials#content">RESIDENTIAL RENDERINGS</a></li>
-								<li <?php $this->helpers->isActiveMenu("interior"); ?>><a
-										href="interior#content">INTERIOR RENDERINGS</a></li>
-								<li <?php $this->helpers->isActiveMenu("floorplan"); ?>><a
-										href="floorplan#content">FLOORPLAN RENDERINGS</a></li>
-								<li <?php $this->helpers->isActiveMenu("permit"); ?>><a href="permit#content">PERMIT TO
-										BUILD RENDERINGS</a></li>
-								<li <?php $this->helpers->isActiveMenu("contractor"); ?>><a
-										href="contractor#content">CONTRACTOR RENDERINGS</a></li>
-								<li <?php $this->helpers->isActiveMenu("virtual"); ?>><a href="virtual#content">VIRTUAL
-										STAGING</a></li>
-							</ul>
-						</nav>
+			<div class="nav_menu">
+				<div class="header_info">
+					<div class="phone">
+						<span class="phone_text">call us at </span>
+						<img src="./public/images/common/header_phone.png" alt="#">
+						<span class="tel_num"> 856 - 749 - 8432 </span>
+					</div>
+					<div class="soc_med">
+						<img src="./public/images/common/soc_med_header.png" alt="#">
 					</div>
 				</div>
+				<nav>
+					<ul>
+						<li><a href="#">HOME</a></li>
+						<li><a href="#">COMMERCIAL RENDERINGS</a></li>
+						<li><a href="#">RESIDENTIAL RENDERINGS</a></li>
+						<li><a href="#">INTERIOR RENDERINGS</a></li>
+						<li><a href="#">FLOORPLAN RENDERINGS</a></li>
+						<li><a href="#">PERMIT TO BUILD RENDERINGS</a></li>
+						<li><a href="#">CONTRACTOR RENDERINGS</a></li>
+						<li><a href="#">VIRTUAL STAGING</a></li>
+					</ul>
+				</nav>
 			</div>
-		</header>
 
-		<div id="home_content">
-			<div class="title">
-				<h4>A CREATIVE TEAM OF</h4>
-				<h5>Real Estate Renderings Specialist</h5>
-				<a href="#">LEARN MORE</a>
-			</div>
+
+
+			<!-- Hero -->
+
 		</div>
-	</section>
-
-	<!-- Hero -->
-	<?php if ($view == "home"): ?>
-
-	<?php endif; ?>
+		<?php if ($view == "home") : ?>
+				<div id="text_container">
+					<h2>A CREATIVE TEAM OF</h2>
+					<h3>Real Estate Renderings Specialists</h3>
+					<a class="btn" href="#">LEARN MORE</a>
+				</div>
+			<?php endif; ?>
+	</header>
