8f61c77a57d4d765b49f4df3a1e298f622a72fa5
webdesign day <a.adriansalguet3.code@gmail.com>
2023-04-06 15:04:04 +0800

    tuloy sa bahay

--- app/view/home.php
+++ app/view/home.php
@@ -2,13 +2,17 @@
     <div class="container">
         <div class="card">
             <div class="text_container">
-            <h2>WHO ARE WE</h2>
-            <h3>Renderings Plus</h3>
-            <h4>Real Estate Marketing Services</h4>
-            <p>
-                We are a Professional Renderings Firm with an Extensive Background in 3D Modeling and in Real Estate Marketing. We Specialize in creating Upscale Elevation Renderings, Interior Renderings, Contractor Renderings, Floorplan Renderings, Permit To Build Renderings, Landscaping Staging, and Virtual Staging. With Years of Experience, delivering only the Highest Quality of Renderings, we’d love to bring your Projects to Life. Please Contact Us for a Free Estimate Today.
-            </p>
-            <p class="tel_num">Call Us Today <span>856-479-8432</span></p>
+                <h2>WHO ARE WE</h2>
+                <h3>Renderings Plus</h3>
+                <h4>Real Estate Marketing Services</h4>
+                <p>
+                    We are a Professional Renderings Firm with an Extensive Background in 3D Modeling and in Real Estate
+                    Marketing. We Specialize in creating Upscale Elevation Renderings, Interior Renderings, Contractor
+                    Renderings, Floorplan Renderings, Permit To Build Renderings, Landscaping Staging, and Virtual
+                    Staging. With Years of Experience, delivering only the Highest Quality of Renderings, we’d love to
+                    bring your Projects to Life. Please Contact Us for a Free Estimate Today.
+                </p>
+                <p class="tel_num">Call Us Today! <span>856-479-8432</span></p>
             </div>
             <a href="#">MORE <img src="./public/images/common/arrow_right.fw.png" alt=""></a>
         </div>
@@ -17,24 +21,107 @@
 
 <section id="about_us">
     <div class="container">
-
+        <img src="./public/images/common/section_3_logo.fw.png" alt="logo">
+        <h2>
+            Renderings that Catch and Hold the Atten of Potential Home Buyers
+        </h2>
+        <h3>Bring Your Designs To Life</h3>
+        <a class="btn" href="#">LEARN MORE</a>
     </div>
 </section>
 
 <section id="what_we_do">
+
+    <div class="text">
+        <h2>WHAT WE DO</h2>
+        <h3>Renderings Plus</h3>
+        <h4>MOST POPULAR</h4>
+    </div>
+
     <div class="container">
+        <div class="gallery">
+
+            <img src="./public/images/common/card_1.fw.png" alt="commercial">
+            <div class="decoration">
+                <figure>
+                    <figcaption>
+                        COMMERCIAL RENDERINGS
+                    </figcaption>
+                </figure>
+                <a href="#">MORE <img src="./public/images/common/arrow_right.fw.png" alt="arrow"></a>
+            </div>
+        </div>
+
+        <div class="gallery">
+            <img src="./public/images/common/card_2.fw.png" alt="commercial">
+            <div class="decoration">
+                <figure>
+                    <figcaption>
+                        COMMERCIAL RENDERINGS
+                    </figcaption>
+                </figure>
+                <a href="#">MORE <img src="./public/images/common/arrow_right.fw.png" alt="arrow"></a>
+            </div>
+        </div>
+
+        <div class="gallery">
+            <img src="./public/images/common/card_3.fw.png" alt="commercial">
+            <div class="decoration">
+                <figure>
+                    <figcaption>
+                        COMMERCIAL RENDERINGS
+                    </figcaption>
+                </figure>
+                <a href="#">MORE <img src="./public/images/common/arrow_right.fw.png" alt="arrow"></a>
+            </div>
+        </div>
 
     </div>
 </section>
 
 <section id="testimonial">
     <div class="container">
-
+        <div class="card">
+            <div class="content">
+                <img src="./public/images/common/quotation.png" alt="">
+                <div class="text_container">
+                    <h2>T E S T I M O N I A L S</h2>
+                    <h3>Renderings Plus</h3>
+                    <p class="reviews">Real Estate Marketing Services Reviews</p>
+                    <div class="stars">
+                        <img src="./public/images/common/stars.png" alt="reviews">
+                        <p> - AI Santiago</p>
+                    </div>
+                    <p class="reviews_content">
+                        professional firm with great renderings
+                        Will use them again in the near future.
+                    </p>
+                    <div class="symbols">
+                        <img src="./public/images/common/arrow_left.fw.png" alt="symbol">
+                        <img src="./public/images/common/arrow_right.fw.png" alt="symbol">
+                    </div>
+                </div>
+            </div>
+            <a href="#">MORE <img src="./public/images/common/arrow_right.fw.png" alt=""></a>
+        </div>
     </div>
 </section>
 
 <section id="gallery">
     <div class="container">
-
+        <div class="gallery_container">
+            <div class="commercial">
+                <h2>COMMERCIAL RENDERINGS</h2>
+                <img src="./public/images/common/commercial_1.png" alt="">
+                <img src="./public/images/common/commercial_2.png" alt="">
+                <img src="./public/images/common/commercial_3.png" alt="">
+            </div>
+            <div class="residential">
+                <h2>RESIDENTIAL RENDERINGS</h2>
+                <img src="./public/images/common/residential_1.png" alt="">
+                <img src="./public/images/common/residential_2.png" alt="">
+                <img src="./public/images/common/residential_3.png" alt="">
+            </div>
+        </div>
     </div>
 </section>
